bdf407324e65eb9f61faa1b7a83e0873c77dfc96
KenStahl <830619@mystma.org>
2020-04-08 18:53:33 +0000

    added top five movies

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -2,6 +2,8 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-    }
-}
+        System.out.println("1.BlackHawkDown");
+        System.out.println("2.Avengers");
+        System.out.println("3.BlackPanther");
+        System.out.println("4.LionKing");
+        System.out.println("5.RemebertheTitans");
