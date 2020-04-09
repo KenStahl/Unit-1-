2878ae71184fc760472b3196ae673d48c21d8c2e
KenStahl <830619@mystma.org>
2020-04-09 19:04:17 +0000

    I Am  Turtle Boolean

--- Unit 1/src/app/App.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("1.BlackHawkDown");
-        System.out.println("2.Avengers");
-        System.out.println("3.BlackPanther");
-        System.out.println("4.LionKing");
-        System.out.println("5.RemebertheTitans");
---
+++ Unit1/src/app/App.java
@@ -0,0 +1,27 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int myFavNum = 46293; 
+        boolean LivesInAlbertville = false;
+       System.out.println("Do I live in Albertville?");
+        System.out.println(LivesInAlbertville);
+        char FirstLetterOfName = 'K';
+        System.out.println(FirstLetterOfName);
+        byte LengthOfACaterpillar = 115;
+        System.out.println(LengthOfACaterpillar);
+        short DiameterOfEarth = 10100;
+        System.out.println(DiameterOfEarth);
+        long  WeightOfAnElephant = 1100000;
+        System.out.println(WeightOfAnElephant);
+        float HeightOfAnAnt = 1.7F;
+        System.out.println(HeightOfAnAnt);
+        double HeightOfSpider = 1.89687d;
+        System.out.println(HeightOfSpider);
+
+        
+
+
+        System.out.println(myFavNum);
+    }
+}
