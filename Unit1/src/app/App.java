329162d603f5c350bbff5dda223bcac4bbd5be47
KenStahl <830619@mystma.org>
2020-04-14 15:36:44 +0000

    MadLib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,27 +1,24 @@
 package app;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        int myFavNum = 46293; 
-        boolean LivesInAlbertville = false;
-       System.out.println("Do I live in Albertville?");
-        System.out.println(LivesInAlbertville);
-        char FirstLetterOfName = 'K';
-        System.out.println(FirstLetterOfName);
-        byte LengthOfACaterpillar = 115;
-        System.out.println(LengthOfACaterpillar);
-        short DiameterOfEarth = 10100;
-        System.out.println(DiameterOfEarth);
-        long  WeightOfAnElephant = 1100000;
-        System.out.println(WeightOfAnElephant);
-        float HeightOfAnAnt = 1.7F;
-        System.out.println(HeightOfAnAnt);
-        double HeightOfSpider = 1.89687d;
-        System.out.println(HeightOfSpider);
-
-        
+        String person1 = "Ken";
+        String person2 = "Dad";
+        String verb1 = "Run";
+        String verb2 = "Sprint";
+        String verb3 = "Chicken";
+        String place1 = "Home";
+        String place2 = "Workplace";
+        String Adjective1 = "delicious";
+        String Adjective2 = "beautiful";
+       
+    
 
 
-        System.out.println(myFavNum);
+        System.out.println(person1 + " " + "is a programmer who likes to" + " " + verb1);
+        System.out.println("every day of the week they make" + " " + Adjective1 + " " + "for breakfast and" + " " + verb2 + " " + "at" + " " + place1 + " " + "with" + person2);
+        System.out.println("sometimes, they go to the" + " " + Adjective2 + " " + place2 + " " + "with" + " " + person2 + " " + "so that they can" + verb3 + " " + "together");
+
     }
 }
