c088b45308c64fd2c011988cdad4e333ddd63334
KenStahl <830619@mystma.org>
2020-04-14 20:03:24 +0000

    Fun Life

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,22 +3,35 @@ package app;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        String person1 = "Ken";
-        String person2 = "Dad";
-        String verb1 = "Run";
-        String verb2 = "Sprint";
-        String verb3 = "Chicken";
-        String place1 = "Home";
-        String place2 = "Workplace";
-        String Adjective1 = "delicious";
-        String Adjective2 = "beautiful";
-       
-    
+      int count = 0; 
+      String color = "blue";
+      
+      
+      
+        while (count < 10) {
+            System.out.println("The count is " + count);
+            count = count + 1; 
+            }
 
+            System.out.println("count finished");
+            while (color == "red"){
+                System.out.println("the color is" + color);
+                color = "green";
 
-        System.out.println(person1 + " " + "is a programmer who likes to" + " " + verb1);
-        System.out.println("every day of the week they make" + " " + Adjective1 + " " + "for breakfast and" + " " + verb2 + " " + "at" + " " + place1 + " " + "with" + person2);
-        System.out.println("sometimes, they go to the" + " " + Adjective2 + " " + place2 + " " + "with" + " " + person2 + " " + "so that they can" + verb3 + " " + "together");
+            }
+            boolean livesinAnoka;
+            while (livesinAnoka = true){
+                System.out.println("I dont live in Anoka");
+                System.out.println(livesinAnoka);
+                livesinAnoka = false;
+        
 
+        
+
+        }
+
+        
+        
     }
 }
+    
