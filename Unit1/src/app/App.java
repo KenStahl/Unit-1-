c166fcf2d3b9b2a898688a409663bc1754d07a19
KenStahl <830619@mystma.org>
2020-04-15 18:55:23 +0000

    Code for 15

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,35 +3,11 @@ package app;
 public class App {
 
     public static void main(String[] args) throws Exception {
-      int count = 0; 
-      String color = "blue";
       
-      
-      
-        while (count < 10) {
-            System.out.println("The count is " + count);
-            count = count + 1; 
-            }
-
-            System.out.println("count finished");
-            while (color == "red"){
-                System.out.println("the color is" + color);
-                color = "green";
-
-            }
-            boolean livesinAnoka;
-            while (livesinAnoka = true){
-                System.out.println("I dont live in Anoka");
-                System.out.println(livesinAnoka);
-                livesinAnoka = false;
-        
-
-        
-
-        }
-
-        
-        
+        for (int i = 0; i <= 10; i = i +1){
+            System.out.println("the count is " + i);
+            System.out.println("let's keep counting.");
+        } 
     }
 }
     
