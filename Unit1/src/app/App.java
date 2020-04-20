52edd38a19a1d4963772180c46d1b38e61cab7ae
KenStahl <830619@mystma.org>
2020-04-20 18:40:56 +0000

    coding

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,13 +1,22 @@
 package app;
 
 public class App {
+        public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) throws Exception {
-      
-        for (int i = 0; i <= 10; i = i +1){
+        for (int i = 22; i <= 28; i = i +3) {
             System.out.println("the count is " + i);
-            System.out.println("let's keep counting.");
-        } 
+            System.out.println("lets keep counting ");
+        }
+        for (int i = 1; i <=9; i = i +1) {
+           System.out.println(i *i);
+            System.out.println(i + 3);
+            System.out.println(i);
+        }
+        for (int i = 50; i >=5; i = i -5) {
+            System.out.println(i);
+        }
+        for (int i = 2; i <= 32; i = i *2){
+            System.out.println(i);
+        }
     }
-}
-    
+} 
