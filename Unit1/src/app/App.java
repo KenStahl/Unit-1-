f8fad2cf72d5f8c41ad09a74e65efa5f0010a916
KenStahl <830619@mystma.org>
2020-04-29 19:39:35 +0000

    coding

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,20 +3,15 @@ package app;
 public class App {
         public static void main(String[] args) throws Exception {
 
-        for (int i = 22; i <= 28; i = i +3) {
-            System.out.println("the count is " + i);
-            System.out.println("lets keep counting ");
-        }
-        for (int i = 1; i <=9; i = i +1) {
-           System.out.println(i *i);
-            System.out.println(i + 3);
-            System.out.println(i);
-        }
-        for (int i = 50; i >=5; i = i -5) {
-            System.out.println(i);
-        }
-        for (int i = 2; i <= 32; i = i *2){
-            System.out.println(i);
-        }
+            int myInt = 0;
+
+            if (myInt < 10){
+                System.out.println("myInt is less than 10");
+            }
+            else{
+                System.out.println("myInt is more than 9");
+            }
+
+            System.out.println("All finished");
     }
 } 
